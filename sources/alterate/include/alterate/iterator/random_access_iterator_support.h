020ea2bdc4fa23a543316706715248a522127371
acc15 <vyacheslav.mayorov@gmail.com>
2014-08-22 13:10:39 +0400

    iterator support. vec optimized

--- sources/alterate/include/alterate/iterator/random_access_iterator_support.h
+++ sources/alterate/include/alterate/iterator/random_access_iterator_support.h
@@ -1,93 +1,137 @@
 #pragma once
 
+#include <boost/iterator/iterator_facade.hpp>
+#include <boost/iterator/reverse_iterator.hpp>
+
+#include <alterate/pattern/crtp_support.h>
+
 namespace alterate {
     namespace iterator {
-
+        
+        //
+        // requires following interface from <Derived> container:
+        //
+        // size_type size() const {return ...; }
+        // reference operator[](size_type const& index) { return ...; }
+        // const reference operator[](size_type const& index) const { return ...; }
+        //
+        
+        template <typename ContainerType, typename DifferenceType = ::alterate::int_t>
+        class random_access_iterator: boost::iterator_facade<
+            random_access_iterator<ContainerType>,
+            typename ContainerType::value_type, 
+            boost::random_access_traversal_tag, 
+            typename ContainerType::value_type&,
+            DifferenceType> 
+        {
+        public:
+            typedef ContainerType                           container_type;
+            typedef typename container_type::size_type      size_type;
+            
+        private:
+            container_type* container_ptr;
+            size_type position;
+            
+        public:
+            random_access_iterator(size_type position = size_type()) : 
+                random_access_iterator::container_ptr(nullptr),
+                random_access_iterator::position(position) {
+            }
+        
+            random_access_iterator(container_type& container, size_type position = size_type()) : 
+                random_access_iterator::container_ptr(&container),
+                random_access_iterator::position(position) {
+            }
+            
+            friend class boost::iterator_core_access;
+            
+            reference dereference() const {
+                return (*container_ptr)[position];
+            }
+            
+            bool_t equal(random_access_iterator const& other) const {
+                return container_ptr == other.container_ptr && position == other.position;
+            }
+            
+            void increment() {
+                ++position;
+            }
+            
+            void decrement() {
+                --position;
+            }
+            
+            void advance(difference_type n) {
+                position += n;
+            }
+            
+            difference_type distance_to(scalar_iterator const& other) const {
+                return other.position - position;
+            }
+        };
+        
         template <typename Derived, 
                   typename ValueType, 
                   typename SizeType = alterate::uint_t, 
                   typename Reference = ValueType&>
-        class random_access_iterator_support {
-        private:
-
-            typedef Derived derived_type;
-
-            derived_type& derived() {
-                return *static_cast<derived_type *>(this);
-            }
-
-            derived_type const& derived() const {
-                return *static_cast<derived_type const*>(this);
-            }
-
-
+        class random_access_iterator_support: private ::alterate::pattern::crtp_support<Derived> {
         public:
             typedef ValueType value_type;
             typedef SizeType size_type;
             typedef Reference reference;
-
-            //
-            // requires following interface from derived():
-            //
-            // size_type size() const {return ...; }
-            // reference operator[](size_type const& index) { return ...; }
-            // const reference operator[](size_type const& index) const { return ...; }
-            //
-
-            // TODO
-//             iterator begin() {
-// 
-//             }
-// 
-//             iterator end() {
-// 
-//             }
-// 
-//             const_iterator begin() const {
-// 
-//             }
-// 
-//             const_iterator end() const {
-// 
-//             }
-// 
-//             const_iterator cbegin() const {
-// 
-//             }
-// 
-//             const_iterator cend() const {
-// 
-//             }
-// 
-// 
-//             reverse_iterator rbegin() {
-// 
-//             }
-// 
-//             reverse_iterator rend() {
-// 
-//             }
-// 
-//             const_reverse_iterator rbegin() const {
-// 
-//             }
-// 
-//             const_reverse_iterator rend() const {
-// 
-//             }
-// 
-//             const_reverse_iterator crbegin() const {
-// 
-//             }
-// 
-//             const_reverse_iterator crend() const {
-// 
-//             }
-
-
+            
+            typedef random_access_iterator<derived_type>        iterator;
+            typedef random_access_iterator<const derived_type>  const_iterator;
+            typedef boost::reverse_iterator<iterator>           reverse_iterator;
+            typedef boost::reverse_iterator<const_iterator>     const_reverse_iterator;
+            
+            iterator begin() {
+                return iterator(derived());
+            }
+            
+            iterator end() {
+                return iterator(derived(), size());
+            }
+            
+            const_iterator begin() const {
+                return const_iterator(derived());
+            }
+            
+            const_iterator end() const {
+                return const_iterator(derived(), size());
+            }
+            
+            const_iterator cbegin() const {
+                return const_iterator(derived());
+            }
+            
+            const_iterator cend() const {
+                return const_iterator(derived(), size());
+            }
+            
+            reverse_iterator rbegin() {
+                return reverse_iterator(end());
+            }
+            
+            reverse_iterator rend() {
+                return reverse_iterator(begin());
+            }
+            
+            const_iterator rbegin() const {
+                return const_reverse_iterator(end());
+            }
+            
+            const_iterator rend() const {
+                return const_reverse_iterator(begin());
+            }
+            
+            const_iterator crbegin() const {
+                return const_reverse_iterator(end());
+            }
+            
+            const_iterator crend() const {
+                return const_reverse_iterator(begin());
+            }
         };
-
-
     }
-
 }
--- sources/alterate/include/alterate/vec.h
+++ sources/alterate/include/alterate/vec.h
@@ -10,185 +10,201 @@
 #include <array>
 
 namespace alterate {
-
+    
     template <typename DerivedType, typename BaseType>
-    struct basic_vec : BaseType, private crtp_support<DerivedType> {
+    struct basic_vec : BaseType, private alterate::pattern::crtp_support<DerivedType> {
         basic_vec() {
-            std::fill(begin(), end(), value_type());
         }
-
+        
         template <typename U>
         basic_vec(U const& v) {
-            *this = v;
+            derived() = v;
         }
-
+        
         template <typename U>
         basic_vec(std::initializer_list<U> const& l) {
-            *this = l;
+            derived() = l;
         }
-
+        
+        template <typename U>
+        derived_type& set(U const& value) {
+            derived() = value;
+        }
+        
+        template <typename U>
+        derived_type& fill(U const& value) {
+            std::fill(begin(), end(), value);
+        }
+        
+        template <typename Func>
+        derived_type& for_each(Func const& func) {
+            std::for_each(begin(), end(), func);
+            return derived();
+        }
+        
         template <typename U, typename Func>
         derived_type& transform(U const& v, Func const& func) {
             typedef alterate::iterable::iterator_provider<U, size_type> iterator_provider;
             alterate::functional::transform_safe(begin(), end(), 
                 iterator_provider::begin(v), iterator_provider::end(v, size()), begin(), func);
-            return get_this();
+            return derived();
         }
-
+        
         template <typename Result, typename U, typename Func>
         Result accumulate(U const& v, Func const& func, Result init) const {
             typedef alterate::iterable::iterator_provider<U, size_type> iterator_provider;
             return alterate::functional::accumulate_safe(begin(), end(), 
                 iterator_provider::begin(v), iterator_provider::end(v, size()), func, init);
         }
-
-        value_type const& comp(uint_t i) const {
-            return get_this()[i];
-        }
-
-        template <typename U>
-        derived_type& comp(uint_t i, U const& v) {
-            get_this()[i] = static_cast<value_type>(v);
-            return get_this();
-        }
-
+        
         derived_type& negate() {
-            std::for_each(begin(), end(), alterate::functional::negate());
-            return get_this();
+            return for_each(alterate::functional::negate());
         }
-
+        
+        value_type const& comp(size_type const& i) const {
+            return derived()[i];
+        }
+        
         template <typename U>
-        value_type dot(U const& v) const {
-            return accumulate(v, alterate::functional::multiply(), value_type());
+        derived_type& comp(size_type const& i, U const& v) {
+            derived()[i] = static_cast<value_type>(v);
+            return derived();
         }
-
-        template <typename U>
-        value_type dot(std::initializer_list<U> const& v) const {
-            return accumulate(v, alterate::functional::multiply(), value_type());
-        }
-
-        value_type length_square() const {
-            return dot(get_this());
-        }
-
-        value_type length() const {
-            return sqrt(length_square());
-        }
-
-        derived_type& normalize() {
-            return get_this() /= length();
-        }
-
-        template <typename U>
-        derived_type& resize(U const& to_length) {
-            return get_this() *= (to_length / length());
-        }
-
+        
         template <typename U>
         derived_type& operator=(U const& v) {
             return transform(v, alterate::functional::return_2nd());
         }
-
+        
         template <typename U>
         derived_type& operator +=(U const& v) {
             return transform(v, alterate::functional::sum());
         }
-
+        
         template <typename U>
         derived_type& operator -=(U const& v) {
             return transform(v, alterate::functional::subtract());
         }
-
+        
         template <typename U>
         derived_type& operator *=(U const& v) {
             return transform(v, alterate::functional::multiply());
         }
-
+        
         template <typename U>
         derived_type& operator /=(U const& v) {
             return transform(v, alterate::functional::divide());
         }
-
+        
         template <typename U>
         derived_type operator +(U const& v) const {
             return make_copy() += v;
         }
-
+        
         template <typename U>
         derived_type operator -(U const& v) const {
             return make_copy() -= v;
         }
-
+        
         template <typename U>
         derived_type operator *(U const& v) const {
             return make_copy() *= v;
         }
-
+        
         template <typename U>
         derived_type operator /(U const& v) const {
             return make_copy() /= v;
         }
-
+        
         template <typename U>
         derived_type& operator=(std::initializer_list<U> const& v) {
             return transform(v, alterate::functional::return_2nd());
         }
-
+        
         template <typename U>
         derived_type& operator+=(std::initializer_list<U> const& v) {
             return transform(v, alterate::functional::sum());
         }
-
+        
         template <typename U>
         derived_type& operator-=(std::initializer_list<U> const& v) {
             return transform(v, alterate::functional::subtract());
         }
-
+        
         template <typename U>
         derived_type& operator*=(std::initializer_list<U> const& v) {
             return transform(v, alterate::functional::multiply());
         }
-
+        
         template <typename U>
         derived_type& operator/=(std::initializer_list<U> const& v) {
             return transform(v, alterate::functional::divide());
         }
-
+        
         template <typename U>
         derived_type operator+(std::initializer_list<U> const& v) const {
             return derived_copy() += v;
         }
-
+        
         template <typename U>
         derived_type operator-(std::initializer_list<U> const& v) const {
             return derived_copy() -= v;
         }
-
+        
         template <typename U>
         derived_type operator*(std::initializer_list<U> const& v) const {
             return derived_copy() *= v;
         }
-
+        
         template <typename U>
         derived_type operator/(std::initializer_list<U> const& v) const {
             return derived_copy() /= v;
         }
-
+        
         derived_type operator-() const {
             return derived_copy().negate();
         }
+        
+        // vector specific operations
+        
+        template <typename U>
+        value_type dot(U const& v) const {
+            return accumulate(v, alterate::functional::multiply(), value_type());
+        }
+        
+        template <typename U>
+        value_type dot(std::initializer_list<U> const& v) const {
+            return accumulate(v, alterate::functional::multiply(), value_type());
+        }
+        
+        value_type length_square() const {
+            return dot(derived());
+        }
+        
+        value_type length() const {
+            return sqrt(length_square());
+        }
+        
+        derived_type& normalize() {
+            return derived() /= length();
+        }
+        
+        template <typename U>
+        derived_type& resize(U const& to_length) {
+            return derived() *= (to_length / length());
+        }
     };
-
+    
     template <uint_t Count, typename T = sp_t>
     class vec : public basic_vec<std::array<T, Count>, vec<Count, T>> {
     public:
         vec() : basic_vec() {
         }
-
+        
         template <typename U>
         vec(U const& v) : basic_vec(v) {
         }
-
+        
         template <typename U>
         vec(std::initializer_list<U> const& l) : basic_vec(l) {
         }
