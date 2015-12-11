c50822a50fe53f21a284e916d70e4792e6949d6e
acc15 <vyacheslav.mayorov@gmail.com>
2015-12-11 03:02:10 +0300

    transform_safe copy_safe tests

--- alterate-math/include/alterate/algorithm.hpp
+++ alterate-math/include/alterate/algorithm.hpp
@@ -2,15 +2,22 @@
 
 namespace alterate {
 
-template <typename TInIter, typename TOutIter>
-TOutIter copy_min(TInIter input_begin, TInIter input_end,
-			  TOutIter output_begin, TOutIter output_end) {
-	for (;input_begin != input_end && output_begin != output_end;
-		input_begin++, output_begin++) {
-		*output_begin = *input_begin;
+template <typename InIter, typename OutIter>
+OutIter copy_safe(InIter in_begin, InIter in_end, OutIter out_begin, OutIter out_end) {
+	for (; in_begin != in_end && out_begin != out_end; in_begin++, out_begin++) {
+		*out_begin = *in_begin;
 	}
-	return output_begin;
+	return out_begin;
 }
+
+template <typename InIter, typename OutIter, typename BinOp>
+OutIter transform_safe(InIter in_begin, InIter in_end, OutIter out_begin, OutIter out_end, BinOp bin_op) {
+	for (; in_begin != in_end && out_begin != out_end; in_begin++, out_begin++) {
+		*out_begin = bin_op(*in_begin, *out_begin);
+	}
+	return out_begin;
+}
+
 
 
 }
--- alterate-math/test/alterate/algorithm.cpp
+++ alterate-math/test/alterate/algorithm.cpp
@@ -4,14 +4,65 @@
 
 #include <alterate/algorithm.hpp>
 
-TEST(algorithm_test, copy_min) {
+TEST(algorithm_test, copy_safe_when_source_is_bigger) {
 
-	std::array<3, int> a;
-	std::array<2, int> b;
+	std::array<int, 3> a = { 10, 11, 12 };
+	std::array<int, 2> b = { 1, 2 };
 
-	alterate::copy_min(a.begin(), a.end(), b.begin(), b.end());
+	alterate::copy_safe(a.begin(), a.end(), b.begin(), b.end());
 
 	ASSERT_EQ(a[0], b[0]);
 	ASSERT_EQ(a[1], b[1]);
+
+}
+
+TEST(algorithm_test, copy_safe_when_source_is_smaller) {
+
+	std::array<int, 2> a = { 10, 11 };
+	std::array<int, 3> b = { 1, 2, 3 };
+
+	alterate::copy_safe(a.begin(), a.end(), b.begin(), b.end());
+
+	ASSERT_EQ(a[0], b[0]);
+	ASSERT_EQ(a[1], b[1]);
+	ASSERT_EQ(3, b[2]);
+
+}
+
+TEST(algorithm_test, transform_safe_add_when_source_is_smaller) {
+
+	std::array<int, 2> a = { 10, 11 };
+	std::array<int, 3> b = { 1, 2, 3 };
+
+	alterate::transform_safe(a.begin(), a.end(), b.begin(), b.end(), [](int x,int y) -> int { return x+y; });
+
+	ASSERT_EQ(11, b[0]);
+	ASSERT_EQ(13, b[1]);
+	ASSERT_EQ(3, b[2]);
+
+}
+
+TEST(algorithm_test, transform_safe_add_when_source_is_bigger) {
+
+	std::array<int, 3> a = { 10, 11, 12 };
+	std::array<int, 2> b = { 1, 2 };
+
+	alterate::transform_safe(a.begin(), a.end(), b.begin(), b.end(), [](int x,int y) -> int { return x+y; });
+
+	ASSERT_EQ(11, b[0]);
+	ASSERT_EQ(13, b[1]);
+
+}
+
+TEST(algorithm_test, transform_safe_add) {
+
+	std::array<int, 3> a = { 10, 11, 12 };
+	std::array<int, 3> b = { 1, 2, 3 };
+
+	alterate::transform_safe(a.begin(), a.end(), b.begin(), b.end(), [](int x,int y) -> int { return x+y; });
+
+	ASSERT_EQ(11, b[0]);
+	ASSERT_EQ(13, b[1]);
+	ASSERT_EQ(15, b[2]);
 
 }
