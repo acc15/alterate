c5649b5139b3e7cd7dddc3cc3d5ff834e6e38ed1
acc15 <vyacheslav.mayorov@gmail.com>
2015-12-11 05:02:04 +0300

    transform_safe copy_safe tests

--- alterate-math/include/alterate/algorithm.hpp
+++ alterate-math/include/alterate/algorithm.hpp
@@ -1,19 +1,24 @@
 #pragma once
 
+#include <iterator>
+
 namespace alterate {
 
 template <typename InIter, typename OutIter>
 OutIter copy_safe(InIter in_begin, InIter in_end, OutIter out_begin, OutIter out_end) {
+	typedef typename std::iterator_traits<OutIter>::value_type out_type;
 	for (; in_begin != in_end && out_begin != out_end; in_begin++, out_begin++) {
-		*out_begin = *in_begin;
+		*out_begin = static_cast<out_type>(*in_begin);
 	}
 	return out_begin;
 }
 
 template <typename InIter, typename OutIter, typename BinOp>
 OutIter transform_safe(InIter in_begin, InIter in_end, OutIter out_begin, OutIter out_end, BinOp bin_op) {
+	typedef typename std::iterator_traits<InIter>::value_type in_type;
+	typedef typename std::iterator_traits<OutIter>::value_type out_type;
 	for (; in_begin != in_end && out_begin != out_end; in_begin++, out_begin++) {
-		*out_begin = bin_op(*in_begin, *out_begin);
+		*out_begin = static_cast<out_type>( bin_op(*in_begin, static_cast<in_type>( *out_begin )) );
 	}
 	return out_begin;
 }
--- alterate-math/test/alterate/algorithm.cpp
+++ alterate-math/test/alterate/algorithm.cpp
@@ -29,6 +29,20 @@ TEST(algorithm_test, copy_safe_when_source_is_smaller) {
 
 }
 
+TEST(algorithm_test, copy_safe_with_different_types) {
+
+	std::array<double, 2> a = { 10.5, 11.3 };
+	std::array<int, 3> b = { 1, 2, 3 };
+
+	alterate::copy_safe(a.begin(), a.end(), b.begin(), b.end());
+
+	ASSERT_EQ(10, b[0]);
+	ASSERT_EQ(11, b[1]);
+	ASSERT_EQ(3, b[2]);
+
+}
+
+
 TEST(algorithm_test, transform_safe_add_when_source_is_smaller) {
 
 	std::array<int, 2> a = { 10, 11 };
@@ -60,6 +74,20 @@ TEST(algorithm_test, transform_safe_add) {
 	std::array<int, 3> b = { 1, 2, 3 };
 
 	alterate::transform_safe(a.begin(), a.end(), b.begin(), b.end(), [](int x,int y) -> int { return x+y; });
+
+	ASSERT_EQ(11, b[0]);
+	ASSERT_EQ(13, b[1]);
+	ASSERT_EQ(15, b[2]);
+
+}
+
+
+TEST(algorithm_test, transform_safe_add_with_different_types) {
+
+	std::array<float, 3> a = { 10.5f, 11.3f, 12.1f };
+	std::array<int, 3> b = { 1, 2, 3 };
+
+	alterate::transform_safe(a.begin(), a.end(), b.begin(), b.end(), [](float x,float y) -> float { return x+y; });
 
 	ASSERT_EQ(11, b[0]);
 	ASSERT_EQ(13, b[1]);
