2c9b8743192d29fbb986b22b219f6a863587d3b4
acc15 <vyacheslav.mayorov@gmail.com>
2015-12-18 04:45:06 +0300

    mingw32 compatibilty

--- alterate-math/include/alterate/algorithm.hpp
+++ alterate-math/include/alterate/algorithm.hpp
@@ -14,8 +14,6 @@ OutIter copy_safe(InIter in_begin, InIter in_end, OutIter out_begin, OutIter out
 
 template <typename InIter, typename OutIter, typename BinOp>
 OutIter transform_safe(InIter in_begin, InIter in_end, OutIter out_begin, OutIter out_end, BinOp bin_op) {
-	typedef typename std::iterator_traits<InIter>::value_type in_type;
-	typedef typename std::iterator_traits<OutIter>::value_type out_type;
 	for (; in_begin != in_end && out_begin != out_end; in_begin++, out_begin++) {
         *out_begin = bin_op(*in_begin, *out_begin);
 	}
