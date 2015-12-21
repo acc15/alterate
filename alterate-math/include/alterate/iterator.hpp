d983ae27424103025d31e54a20cafa348f2d75f6
acc15 <vyacheslav.mayorov@gmail.com>
2015-12-21 03:34:40 +0300

    iterator tests

--- alterate-math/include/alterate/iterator.hpp
+++ alterate-math/include/alterate/iterator.hpp
@@ -63,22 +63,86 @@ private:
     }
 };
 
-template <typename T>
-struct container_iterator_factory {
+template <typename ArrayType>
+struct array_iterator_factory {
 
-    typedef T container_type;
-    typedef typename container_type::const_iterator const_iterator;
+    enum { ARRAY_SIZE = std::extent<ArrayType>::value };
 
-    static const_iterator begin(const T& container) {
-        return container.begin();
+    typedef typename std::add_pointer<
+            typename std::add_const<typename std::remove_extent<ArrayType>::type>::type>::type const_iterator;
+
+    static const_iterator begin(const ArrayType& v) {
+        return v;
     }
 
-    static const_iterator end(const T& container, size_t /*size*/) {
-        return container.end();
+    static const_iterator end(const ArrayType& v, size_t /*unused*/) {
+        return v + ARRAY_SIZE;
+    }
+};
+
+template <typename PointerType>
+struct pointer_iterator_factory {
+
+    typedef typename std::add_pointer<
+            typename std::add_const<typename std::remove_pointer<PointerType>::type>::type>::type const_iterator;
+
+    static const_iterator begin(const PointerType& v) {
+        return v;
+    }
+
+    static const_iterator end(const PointerType& v, size_t size) {
+        return v + size;
+    }
+};
+
+
+template <typename ContainerType>
+struct container_iterator_factory {
+
+    typedef typename ContainerType::const_iterator const_iterator;
+
+    static const_iterator begin(const ContainerType& v) {
+        return v.begin();
+    }
+
+    static const_iterator end(const ContainerType& v, size_t) {
+        return v.end();
+    }
+};
+
+/*
+template <typename ScalarType>
+struct scalar_iterator_factory {
+
+    typedef scalar_iterator<const ScalarType> const_iterator;
+
+    static const_iterator begin(const ScalarType& v) {
+        return const_iterator(v, 0);
+    }
+
+    static const_iterator end(const ScalarType& v, size_t size) {
+        return const_iterator(v, static_cast<const_iterator::difference_type>(size));
     }
 
 };
 
+// metafunction
+template <typename ContainerType>
+struct get_iterator_factory {
 
+    typedef typename std::conditional<std::is_array<ContainerType>::value, array_iterator_factory<ContainerType>,
+            typename std::conditional<std::is_pointer<ContainerType>::value, pointer_iterator_factory<ContainerType>,
+            typename std::conditional<std::is_scalar<ContainerType>::value, scalar_iterator_factory<ContainerType>,
+            container_iterator_factory<ContainerType> >::type >::type >::type value;
 
+};
+
+template <typename ContainerType>
+struct iterator_factory: get_iterator_factory<ContainerType>::value {
+
+    typedef typename get_iterator_factory<ContainerType>::value factory_type;
+    typedef typename factory_type::const_iterator const_iterator;
+
+};
+*/
 }
--- alterate-math/test/alterate/iterator.cpp
+++ alterate-math/test/alterate/iterator.cpp
@@ -49,3 +49,54 @@ TEST(iterator_test, scalar_iterator_equality) {
 	ASSERT_EQ(iter1, iter4);
 
 }
+
+TEST(iterator_test, array_iterator_factory_test) {
+
+	typedef alterate::array_iterator_factory< int[10] > factory_type;
+	typedef typename factory_type::const_iterator const_iterator;
+
+	int test_array[10] = {1,2,3,4,5,6,7,8,9,10};
+
+	const_iterator begin = factory_type::begin(test_array);
+	const_iterator end = factory_type::end(test_array, 0);
+
+	int dist = std::distance(begin, end);
+	ASSERT_EQ(10, dist);
+	ASSERT_EQ(1, *begin);
+
+	++begin;
+	ASSERT_EQ(2, *begin);
+
+	begin += 8;
+	ASSERT_EQ(10, *begin);
+
+	++begin;
+	ASSERT_EQ(begin, end);
+
+}
+
+TEST(iterator_test, pointer_iterator_factory_test) {
+
+	typedef alterate::pointer_iterator_factory< int* > factory_type;
+	typedef typename factory_type::const_iterator const_iterator;
+
+	int test_array[10] = {1,2,3,4,5,6,7,8,9,10};
+
+	const_iterator begin = factory_type::begin(test_array);
+	const_iterator end1 = factory_type::end(test_array, 0);
+	const_iterator end2 = factory_type::end(test_array, 5);
+
+	ASSERT_EQ(begin,end1);
+	ASSERT_NE(begin,end2);
+
+
+	std::vector<int> expected = {1,2,3,4,5,6,7,8,9,10};
+	std::vector<int> actual;
+
+	ASSERT_EQ(1, *begin);
+	ASSERT_EQ(6, *end2);
+
+	ASSERT_EQ(5, std::distance(begin, end2));
+
+}
+
